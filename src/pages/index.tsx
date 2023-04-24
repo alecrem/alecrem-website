b4af7fda3daf6588e09642d4887cec528e10fe7d
Alejandro Cremades <ale@alecrem.com>
2023-04-24 22:30:05 +0900

    Path aliases for simpler imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,12 @@ import {
 } from '@chakra-ui/react'
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Hero } from '@/components/Hero'
+import { Container } from '@/components/Container'
+import { Main } from '@/components/Main'
+import { DarkModeSwitch } from '@/components/DarkModeSwitch'
+import { CTA } from '@/components/CTA'
+import { Footer } from '@/components/Footer'
 
 const Index = () => (
   <Container height="100vh">
