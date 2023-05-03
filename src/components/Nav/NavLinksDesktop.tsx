24e3fce9802a0b312c8fd1b71fbc6eecf7d35dfc
Alejandro Cremades <ale@alecrem.com>
2023-05-04 01:48:23 +0900

    Fix hydration error

--- src/components/Nav/NavLinksDesktop.tsx
+++ src/components/Nav/NavLinksDesktop.tsx
@@ -9,28 +9,28 @@ const NavLinksDesktop: FC = () => {
     <>
       <Box mr={4}>
         <NextLink passHref href="/" locale={router.locale}>
-          <Button as="a" variant="solid" aria-label="Home" w="100%">
+          <Button variant="solid" aria-label="Home" w="100%">
             Home
           </Button>
         </NextLink>
       </Box>
       <Box mr={4}>
         <NextLink passHref href="/skills" locale={router.locale}>
-          <Button as="a" variant="solid" aria-label="Skills" w="100%">
+          <Button variant="solid" aria-label="Skills" w="100%">
             Skills
           </Button>
         </NextLink>
       </Box>
       <Box mr={4}>
         <NextLink passHref href="/projects" locale={router.locale}>
-          <Button as="a" variant="solid" aria-label="Projects" w="100%">
+          <Button variant="solid" aria-label="Projects" w="100%">
             Projects
           </Button>
         </NextLink>
       </Box>
       <Box mr={4}>
         <NextLink passHref href="/contact" locale={router.locale}>
-          <Button as="a" variant="solid" aria-label="Contact" w="100%">
+          <Button variant="solid" aria-label="Contact" w="100%">
             Contact
           </Button>
         </NextLink>
