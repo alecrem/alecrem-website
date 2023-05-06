567b8be4b168dd70f1f678280ab81948cdf29011
Alejandro Cremades <ale@alecrem.com>
2023-05-06 11:37:59 +0900

    Localize headings on Skills page

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,17 +1,8 @@
-import {
-  Heading,
-  List,
-  ListIcon,
-  Tag,
-  TagLabel,
-  TagLeftIcon,
-  ListItem,
-  Wrap,
-  WrapItem
-} from '@chakra-ui/react'
+import { Heading, List, ListIcon, ListItem, Wrap } from '@chakra-ui/react'
 import { CheckCircleIcon } from '@chakra-ui/icons'
 import { Layout } from '@/components/Layout'
 import { SkillTag } from '@/components/SkillTag'
+import useTranslation from 'next-translate/useTranslation'
 
 const skillList = {
   naturalLanguages: [
@@ -57,47 +48,50 @@ const skillList = {
   ]
 }
 
-const Skills = () => (
-  <Layout title="Skills | alecrem">
-    <Heading as="h3" size="lg" color="text" mt={16}>
-      Skills
-    </Heading>
-    <Heading as="h4" size="md" color="text">
-      Natural Languages
-    </Heading>
-    <List spacing={3} my={0} color="text">
-      {skillList.naturalLanguages.map((language, idx) => (
-        <ListItem key={'languages-' + idx}>
-          <ListIcon as={CheckCircleIcon} color="cyan.500" />
-          {language.language}: {language.level}
-        </ListItem>
-      ))}
-    </List>
-    <Heading as="h4" size="md" color="text">
-      Web
-    </Heading>
-    <Wrap>
-      {skillList.web.map((skill, idx) => (
-        <SkillTag key={'web-' + idx}>{skill}</SkillTag>
-      ))}
-    </Wrap>
-    <Heading as="h4" size="md" color="text">
-      Web3
-    </Heading>
-    <Wrap>
-      {skillList.web3.map((skill, idx) => (
-        <SkillTag key={'web3-' + idx}>{skill}</SkillTag>
-      ))}
-    </Wrap>
-    <Heading as="h4" size="md" color="text">
-      Data and ML
-    </Heading>
-    <Wrap>
-      {skillList.dataMl.map((skill, idx) => (
-        <SkillTag key={'data-ml-' + idx}>{skill}</SkillTag>
-      ))}
-    </Wrap>
-  </Layout>
-)
+const Skills: React.FC = () => {
+  const { t } = useTranslation('common')
+  return (
+    <Layout title="Skills | alecrem">
+      <Heading as="h3" size="lg" color="text" mt={16}>
+        {t('skills.title')}
+      </Heading>
+      <Heading as="h4" size="md" color="text">
+        {t('skills.natural-languages')}
+      </Heading>
+      <List spacing={3} my={0} color="text">
+        {skillList.naturalLanguages.map((language, idx) => (
+          <ListItem key={'languages-' + idx}>
+            <ListIcon as={CheckCircleIcon} color="cyan.500" />
+            {language.language}: {language.level}
+          </ListItem>
+        ))}
+      </List>
+      <Heading as="h4" size="md" color="text">
+        {t('skills.web')}
+      </Heading>
+      <Wrap>
+        {skillList.web.map((skill, idx) => (
+          <SkillTag key={'web-' + idx}>{skill}</SkillTag>
+        ))}
+      </Wrap>
+      <Heading as="h4" size="md" color="text">
+        {t('skills.web3')}
+      </Heading>
+      <Wrap>
+        {skillList.web3.map((skill, idx) => (
+          <SkillTag key={'web3-' + idx}>{skill}</SkillTag>
+        ))}
+      </Wrap>
+      <Heading as="h4" size="md" color="text">
+        {t('skills.data-ml')}
+      </Heading>
+      <Wrap>
+        {skillList.dataMl.map((skill, idx) => (
+          <SkillTag key={'data-ml-' + idx}>{skill}</SkillTag>
+        ))}
+      </Wrap>
+    </Layout>
+  )
+}
 
 export default Skills
