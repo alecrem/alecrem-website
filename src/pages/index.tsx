84658cbe239eedf4c386d03351b7cc77d15cb2b6
Alejandro Cremades <ale@alecrem.com>
2023-05-06 14:58:04 +0900

    Localized Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,70 +7,73 @@ import {
 } from '@chakra-ui/react'
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 import { Layout } from '@/components/Layout'
+import useTranslation from 'next-translate/useTranslation'
 
-const Index = () => (
-  <Layout tagline={true} title="alecrem | Alejandro Cremades Rocamora">
-    <Heading as="h4" size="md" color="text">
-      Current occupations
-    </Heading>
-    <List spacing={3} my={0} color="text">
-      <ListItem>
-        <ListIcon as={CheckCircleIcon} color="cyan.500" />
-        <ChakraLink
-          isExternal
-          href="https://limu.ait.kyushu-u.ac.jp/e/"
-          flexGrow={1}
-          mr={2}
-        >
-          Kyushu University Laboratory for Image and Media Understanding{' '}
-          <LinkIcon />
-        </ChakraLink>
-      </ListItem>
-      <ListItem>
-        <ListIcon as={CheckCircleIcon} color="cyan.500" />
-        <ChakraLink
-          isExternal
-          href="https://studiokura.info/en/"
-          flexGrow={1}
-          mr={2}
-        >
-          Studio Kura <LinkIcon />
-        </ChakraLink>
-      </ListItem>
-    </List>
-    <Heading as="h4" size="md" color="text">
-      Memberships
-    </Heading>
-    <List spacing={3} my={0} color="text">
-      <ListItem>
-        <ListIcon as={CheckCircleIcon} color="cyan.500" />
-        <ChakraLink
-          isExternal
-          href="https://henkaku.org/ja/henkaku-community/"
-          flexGrow={1}
-          mr={2}
-        >
-          Chiba Institute of Technology Henkaku Community <LinkIcon />
-        </ChakraLink>
-      </ListItem>
-    </List>
-    <Heading as="h4" size="md" color="text">
-      Publications
-    </Heading>
-    <List spacing={3} my={0} color="text">
-      <ListItem>
-        <ListIcon as={CheckCircleIcon} color="cyan.500" />
-        <ChakraLink
-          isExternal
-          href="https://amzn.to/3V7imAm"
-          flexGrow={1}
-          mr={2}
-        >
-          どんどん話せるスペイン語 作文トレーニング (2016) <LinkIcon />
-        </ChakraLink>
-      </ListItem>
-    </List>
-  </Layout>
-)
+const Index: React.FC = () => {
+  const { t } = useTranslation('common')
+  return (
+    <Layout tagline={true} title="alecrem | Alejandro Cremades Rocamora">
+      <Heading as="h4" size="md" color="text">
+        {t('home.occupations.title')}
+      </Heading>
+      <List spacing={3} my={0} color="text">
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="cyan.500" />
+          <ChakraLink
+            isExternal
+            href={t('home.occupations.limu-url')}
+            flexGrow={1}
+            mr={2}
+          >
+            {t('home.occupations.limu')} <LinkIcon />
+          </ChakraLink>
+        </ListItem>
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="cyan.500" />
+          <ChakraLink
+            isExternal
+            href={t('home.occupations.kura-url')}
+            flexGrow={1}
+            mr={2}
+          >
+            {t('home.occupations.kura')} <LinkIcon />
+          </ChakraLink>
+        </ListItem>
+      </List>
+      <Heading as="h4" size="md" color="text">
+        {t('home.memberships.title')}
+      </Heading>
+      <List spacing={3} my={0} color="text">
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="cyan.500" />
+          <ChakraLink
+            isExternal
+            href={t('home.memberships.henkaku-url')}
+            flexGrow={1}
+            mr={2}
+          >
+            {t('home.memberships.henkaku')} <LinkIcon />
+          </ChakraLink>
+        </ListItem>
+      </List>
+      <Heading as="h4" size="md" color="text">
+        {t('home.publications.title')}
+      </Heading>
+      <List spacing={3} my={0} color="text">
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="cyan.500" />
+          <ChakraLink
+            isExternal
+            href={t('home.publications.dondon-url')}
+            flexGrow={1}
+            mr={2}
+          >
+            {t('home.publications.dondon')} <LinkIcon />
+          </ChakraLink>
+        </ListItem>
+      </List>
+    </Layout>
+  )
+}
 
 export default Index
